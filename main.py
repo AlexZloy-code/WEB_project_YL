d398c9181ccba7ced8d2e471293df03c1e8dcd84
AlexZloy-code <SashaZloy7@yandex.ru>
2025-05-15 18:51:17 +0300

    красивость

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import datetime
 import random as r
 
-from flask import Flask, render_template, redirect, request, jsonify, make_response, session, url_for, render_template_string
+from game_files.saper_web import Minesweeper
+from game_files.diction_for_b_a_c import nums
+from game_files.cat_catch import CatCatch
+
+from flask import Flask, render_template, redirect, request, jsonify, make_response, session, url_for
+from forms.user import LoginForm, RegisterForm
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_session import Session
 
 from data import db_session
 from data.users import User
-from forms.user import LoginForm, RegisterForm
 
-from saper_web import Minesweeper
-from cat_catch import CatCatch
 
 app = Flask(__name__)
 app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
@@ -34,7 +36,7 @@ def main():
 @app.route("/index")
 def index():
     db_sess = db_session.create_session()
-    return render_template("index.html", title='Главная')
+    return render_template("mainfiles/index.html", title='Главная')
 
 
 #      БЫКИ И КОРОВЫ
@@ -72,13 +74,10 @@ def make_guess():
             print('win')
             session.pop('b_a_c', None)
             return jsonify({'html': '<h3>Вы выиграли!</h3>', 'status': 'win'})
-            
         else:
-            while True:
-                proba = str(r.randint(10 ** (digits - 1), 10 ** digits) - 1)
+            for proba in nums[str(digits)]:
+                proba = str(proba)
                 if proba in [i[0] for i in session["b_a_c"]['attempts']]:
-                    continue
-                if len(set(proba)) != len(proba):
                     continue
                 flag = True
                 for ans in session["b_a_c"]['attempts']:
@@ -95,7 +94,7 @@ def make_guess():
                     break
             session["b_a_c"]['num'] = proba
         #a = {'html': ''.join(['<tr>' + ''.join([f'<td>{i}</td>' for i in j]) + '</tr>' for j in session['b_a_c']['attempts'][::-1]]), 'status': 'guess'}
-        return jsonify({'html': ''.join(['<tr>' + ''.join([f'<td>{i}</td>' for i in j]) + '</tr>'
+    return jsonify({'html': ''.join(['<tr>' + ''.join([f'<td>{i}</td>' for i in j]) + '</tr>'
                                                   for j in session['b_a_c']['attempts'][::-1]]),
                                                   'status': 'guess'})
 
@@ -104,7 +103,7 @@ def make_guess():
 def bulls_and_cows():
     if 'b_a_c' in session:
         if request.method == 'GET':
-            return render_template('b_a_c_game.html',
+            return render_template('b_a_c/b_a_c_game.html',
                                    history=session["b_a_c"]['attempts'],
                                    digits=session['b_a_c']['digits'])
 
@@ -114,6 +113,8 @@ def bulls_and_cows():
         digits = int(request.form.get('digits', 4))
         difficulty = request.form.get('difficulty', 'easy')
         proba = str(r.randint(10**(digits-1), 10**digits - 1))
+        while len(set(proba)) != digits:
+            proba = str(r.randint(10**(digits-1), 10**digits - 1))
         
         session["b_a_c"] = {
             'num': proba,
@@ -126,7 +127,7 @@ def bulls_and_cows():
 
         return redirect(url_for('bulls_and_cows'))
     
-    return render_template('b_a_c_menu.html')
+    return render_template('b_a_c/b_a_c_menu.html')
 
 
 #          САПЕР
@@ -134,8 +135,8 @@ def bulls_and_cows():
 def minesweeper():
     if "minesweeper" in session:
         game = session["minesweeper"]
-        return render_template('minesweeper.html', field=game.transformed_matrix, title='Сапер')
-    return render_template('minesweeper.html', start=True, field=[], title='Сапер')
+        return render_template('minesweeper/minesweeper.html', field=game.transformed_matrix, title='Сапер')
+    return render_template('minesweeper/minesweeper.html', start=True, field=[], title='Сапер')
 
 @app.route('/minesweeper/start', methods=['POST'])
 def start_game():
@@ -145,7 +146,7 @@ def start_game():
         session["minesweeper"] = Minesweeper(data['field'])  # Создаём игру один раз для сессии
     game = session["minesweeper"]
 
-    return jsonify({'field': render_template('minesweeper.html', field=game.transformed_matrix, title='Сапер')})
+    return jsonify({'field': render_template('minesweeper/minesweeper.html', field=game.transformed_matrix, title='Сапер')})
 
 
 @app.route('/minesweeper/select-cell', methods=['POST'])
@@ -172,11 +173,11 @@ def interact_cell():
         print(msg)
         session.pop('minesweeper', None)
         return jsonify({'message': msg,
-                        'upd_field': render_template('mineField.html', field=field)})
+                        'upd_field': render_template('minesweeper/mineField.html', field=field)})
     
-    print(f'Try to {data['interact_type']} cell {game.selected_cell_id}')
+    print(f'Try to {data["interact_type"]} cell {game.selected_cell_id}')
     return jsonify({'message': '',
-                    'upd_field': render_template('mineField.html', field=field)})
+                    'upd_field': render_template('minesweeper/mineField.html', field=field)})
 
 
 #       ПОЙМАЙ КОТА
@@ -184,10 +185,10 @@ def interact_cell():
 def cat_catch():
     if "catch_cat" in session:
         game = session["catch_cat"]
-        return render_template('catch_cat.html', field=game.transformed_matrix, title='Поймай кота!')
+        return render_template('catch/catch_cat.html', field=game.transformed_matrix, title='Поймай кота!')
     session['catch_cat'] = CatCatch()
     game = session['catch_cat']
-    return render_template('catch_cat.html', field=game.transformed_matrix, title='Поймай кота!')
+    return render_template('catch/catch_cat.html', field=game.transformed_matrix, title='Поймай кота!')
 
 
 @app.route('/catch_a_cat/interact', methods=['POST'])
@@ -202,7 +203,7 @@ def cat_interact_cell():
         add_class = ' move-lefter'
         if x % 2 == 0:
             add_class = ''
-        field[x] = f'<div class="row{add_class}">{''.join(field[x])}</div>'
+        field[x] = f'<div class="row{add_class}">{"".join(field[x])}</div>'
     field = ''.join(field)
 
     # При выигрыше/проигрыше очищаются данные сессии
@@ -214,6 +215,18 @@ def cat_interact_cell():
     print(f'Move to cell {game.cat.cords}')
     return jsonify({'field': field, 'status': game_response['status']})
 
+
+# Очистка сессии
+@app.route('/clear_game_session', methods=['POST'])
+def clear_game_session():
+    data = request.get_json()
+    session.pop(data['session'], None)
+    return 'success'
+
+# Окно с играми
+@app.route('/games')
+def games():
+    return render_template('mainfiles/games_menu.html', title='Игры')
 
 # Дальше всякая муть с логинами и регистрациями
 @login_manager.user_loader
@@ -232,10 +245,10 @@ def login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
-        return render_template('login.html',
+        return render_template('mainfiles/login.html',
                                message="Неправильный логин или пароль",
                                form=form)
-    return render_template('login.html', title='Авторизация', form=form)
+    return render_template('mainfiles/login.html', title='Авторизация', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -243,12 +256,12 @@ def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
+            return render_template('mainfiles/register.html', title='Регистрация',
                                    form=form,
                                    message="Пароли не совпадают")
         db_sess = db_session.create_session()
         if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
+            return render_template('mainfiles/register.html', title='Регистрация',
                                    form=form,
                                    message="Такой пользователь уже есть")
         user = User(
@@ -263,15 +276,7 @@ def reqister():
         db_sess.commit()
         login_user(user, remember=True)
         return redirect('/')
-    return render_template('register.html', title='Регистрация', form=form)
-
-
-# Обработчик очистки сессии для отдельной игры
-@app.route('/clear_game_session', methods=['POST'])
-def clear_game_session():
-    data = request.get_json()
-    session.pop(data['session'], None)
-    return 'success'
+    return render_template('mainfiles/register.html', title='Регистрация', form=form)
 
 
 @app.route('/logout')
